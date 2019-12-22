76c9cf7e6a8fd77425182ec0099295864ea8a72c
andrews1991 <53892375+andrews1991@users.noreply.github.com>
2019-12-23 00:31:21 +0530

    event Dashboard changes

--- StudentPortal/src/main/resources/static/js/eventDashboard.js
+++ StudentPortal/src/main/resources/static/js/eventDashboard.js
@@ -17,16 +17,16 @@ function callEventsbyType(){
 			for(var i=0;i<rowObj.length;i++){
 			temp++;
 			
-			samp='<div class="col-md"><div class="card card-cascade narrower" style="padding:1%;"> <div class="view view-cascade overlay">';
+			samp='<div class="col-md-4"><div class="card card-cascade narrower" style="padding:1%;"> <div class="view view-cascade overlay">';
 			samp+='<img  class="card-img-top" src="data:image/png;base64,'+rowObj[i].eventImage+'" alt="Card image cap"><a><div class="mask rgba-white-slight"></div></a></div><div class="card-body card-body-cascade text-center">';
 			samp+='<h4 class="card-title"><strong>'+rowObj[i].eventName+'</strong></h4>';
 			samp+=   '<h5 class="blue-text pb-2"><strong>'+rowObj[i].eventOrgName+'</strong></h5>';
 			samp+=  '<p class="card-text">'+rowObj[i].eventDescription+'</p>';
 			samp+='<a class="btn btn-unique">Book Now</a> <a class="px-2 fa-lg li-ic"><i class="fab fa-linkedin-in"></i></a><a class="px-2 fa-lg tw-ic"><i class="fab fa-twitter"></i></a><a class="px-2 fa-lg fb-ic"><i class="fab fa-facebook-f"></i></a></div></div></div></div>';
 			
-			if(temp%3==0 || i==0){
+			/*if(temp%3==0 || i==0){
 				samp='<div class="row" style="padding:1%;">'+samp+'</div>';
-			}
+			}*/
 			final+=samp;
 			}
 			//alert(final);
